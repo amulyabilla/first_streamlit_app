d66c597ff1c9992b224aadef9c356f0d94c3d7f5
amulyabilla <35063389+amulyabilla@users.noreply.github.com>
2022-06-09 15:29:58 -0400

    Update streamlit_app.py
    Added another header for smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast menu')
 
 streamlit.text('🍉🍓🥝🍅🍒🍑🍍🐔 🥑Poori Idly Dosa Chapathi Rava Dosa Uthappam Avacado toast')
+
+streamlit.header('🍌🍍 Build Your Own Fruit Smoothie 🥝🍇')
