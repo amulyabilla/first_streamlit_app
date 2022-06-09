182ba46aae6cfb02f3980e6b549cb94025509386
amulyabilla <35063389+amulyabilla@users.noreply.github.com>
2022-06-09 15:19:34 -0400

    Update streamlit_app.py
    Added Breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+
+streamlit.title('Breakfast menu 
+                Poori
+                Idly
+                Dosa
+                Chapathi
+                Rava Dosa
+                Uthappam')
